edba0fe652af8b88de76f94b1b43b26bb982c612
Raghav <raghavawasthi2005@gmail.com>
2024-12-30 21:50:59 +0400

    fixed errors with input id name and autoComplete missing fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,9 @@ import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 async function connectSerial() { // Connect to ESP32 (cu.wchuusbserial)
+  console.log("connectSerial called");
   const log = document.getElementById('target');
-    
+
   try {
     const port = await navigator.serial.requestPort();
     await port.open({ baudRate: 9600 });
@@ -36,12 +37,16 @@ async function connectSerial() { // Connect to ESP32 (cu.wchuusbserial)
           reader.releaseLock();
           break;
         }
+      } else {
+        console.log('[readLoop] value: N/A length: N/A');
       }
 
       if (done) {
         console.log('[readLoop] DONE', done);
         reader.releaseLock();
         break;
+      } else {
+        console.log('[readLoop] NOT DONE', done);
       }
     }
 
@@ -72,6 +77,8 @@ export default function Home() {
   }
 
   function handleConnect() {
+    console.log("handleConnect called");
+    console.log("navigator.serial", navigator.serial);
     if (navigator.serial) {
       connectSerial().then(address => {
         if (address) {
@@ -86,21 +93,51 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{ fontFamily: 'IBM Plex Sans, sans-serif' }}>
-      <h1 className="text-4xl">
-        Device Registration
-      </h1>
+      <h1 className="text-4xl">Device Registration</h1>
       <Stack className="sm:p-20">
-        <Input placeholder="MAC Address" variant="flushed" value={macAddress} readOnly/>
-        {!connected && (<Button variant="surface" width="100%" onClick={handleConnect}>
-          Connect to device
-        </Button>)}
-        <Input placeholder="Username" variant="flushed" onChange={(e) => setUsername(e.target.value)}/>
-        <Input placeholder="Password" variant="flushed" onChange={(e) => setPassword(e.target.value)}/>
-        <Button variant="surface" className="mt-4" width="100%" onClick={handleRegister}>
+        {/* MAC address is read-only, so we can turn off autocomplete */}
+        <Input
+          id="macAddress"
+          name="macAddress"
+          placeholder="MAC Address"
+          variant="flushed"
+          autoComplete="off"
+          value={macAddress}
+          readOnly
+        />
+        {!connected && (
+          <Button variant="surface" width="100%" onClick={handleConnect}>
+            Connect to device
+          </Button>
+        )}
+        {/* Username: let browser autofill using autoComplete="username" */}
+        <Input
+          id="username"
+          name="username"
+          placeholder="Username"
+          variant="flushed"
+          autoComplete="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        {/* Password: let browser autofill using autoComplete="current-password" (or "new-password" if appropriate) */}
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          variant="flushed"
+          autoComplete="current-password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          variant="surface"
+          className="mt-4"
+          width="100%"
+          onClick={handleRegister}
+        >
           Register
         </Button>
       </Stack>
-      
     </div>
   );
 }
