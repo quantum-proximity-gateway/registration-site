461737f97ffae61a8e34266401cc00e230b1744a
Marwan <yassinimarwan6@gmail.com>
2025-02-24 17:53:38 +0000

    added submition button, record button

--- src/app/registerFace/page.tsx
+++ src/app/registerFace/page.tsx
@@ -1,46 +1,83 @@
 'use client'
 
 import React, { useRef, useEffect, useState } from 'react';
-import  {Text} from "@chakra-ui/react";
+import  {Button, Text, Box} from "@chakra-ui/react";
 
 
 const RegisterFace = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [countdown, setCountdown] = useState(5); // 5 seconds of recording
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [countdown, setCountdown] = useState(0);
+    const [record, setRecord] = useState(false);
+    const [recorded, setRecorded] = useState(false);
 
-  
+    useEffect(() => {
+        if (!record) return;
 
-  useEffect(() => {
-    const getVideo = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
+        const getVideo = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+            }
+            const countdownInterval = setInterval(() => {
+                setCountdown(prevCountdown => {
+                if (prevCountdown <= 1) {
+                    clearInterval(countdownInterval);
+                    stream.getTracks().forEach(track => track.stop());
+                    setRecord(false);
+                    setRecorded(true);
+                }
+                return prevCountdown - 1;
+                });
+            }, 1000);
+        } catch (err) {
+            console.error("Error accessing webcam: ", err);
         }
-        const countdownInterval = setInterval(() => {
-            setCountdown(prevCountdown => {
-              if (prevCountdown <= 1) {
-                clearInterval(countdownInterval);
-                stream.getTracks().forEach(track => track.stop());
-              }
-              return prevCountdown - 1;
-            });
-          }, 1000);
-      } catch (err) {
-        console.error("Error accessing webcam: ", err);
-      }
+        };
+
+        getVideo();
+    }, [record]);
+
+    return (
+    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh" fontFamily="IBM Plex Sans, sans-serif">
+      {!record ? (
+        <>
+          <Text as="h1" fontSize="4xl" mb={4}>Face Registration</Text>
+          <Text as="h2" fontSize="xl" mb={4}>Look into the camera for 5 seconds</Text>
+          <Button 
+            onClick={() => {setRecord(true); setCountdown(5);}} 
+            colorScheme="teal" 
+            size="lg" 
+            mb={4}
+            borderRadius="md"
+            boxShadow="md"
+            px="8"
+          >
+            Record
+          </Button>
+          {recorded ? (
+            <>
+            <Button 
+            colorScheme="teal" 
+            size="lg" 
+            mb={4}
+            borderRadius="md"
+            boxShadow="md"
+            px="8"
+          >
+            Submit face registration
+          </Button>
+          </>
+          ) : <></>}
+        </>
+      ) : (
+        <Box as="video" borderRadius="10px" ref={videoRef} autoPlay mb={4} />
+      )}
+      {countdown > 0 ? <Text as="h2" fontSize="xl">{countdown}</Text> : null}
+      
+     
+    </Box>
+    );
     };
 
-    getVideo();
-  }, []);
-
-  return (
-    <div style={{ display: 'flex', flexDirection:'column',justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'IBM Plex Sans, sans-serif' }}>
-        <h1>User Registration</h1>
-        <video style={{borderRadius:'10px'}}ref={videoRef} autoPlay />
-        <h2>{countdown}</h2>
-    </div>
-  );
-};
-
-export default RegisterFace;
+    export default RegisterFace;
