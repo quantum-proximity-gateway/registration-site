a43230f39832eab69c25cc50c280aeda14755259
Marwan <yassinimarwan6@gmail.com>
2025-03-03 00:18:20 +0000

    cleaned up encrypt_data function

--- oqs-wasm/src/lib.rs
+++ oqs-wasm/src/lib.rs
@@ -52,23 +52,5 @@ pub fn generate_shared_secret(data: &JsValue) -> JsValue {
 
 #[wasm_bindgen]
 pub fn encrypt_data(data: &JsValue) -> JsValue {
-    console_error_panic_hook::set_once(); // Helps with debugging errors in WASM
-    let input: SharedSecretInput = serde_wasm_bindgen::from_value(data.clone()).unwrap();
-    let decoded_pk: Vec<u8> = BASE64_STANDARD.decode(input.public_key_b64.clone()).unwrap();
-
-    // Create ciphertext and shared_secret arrays
-    let mut ciphertext: Vec<u8> = vec![0; OQS_KEM_kyber_512_length_ciphertext as usize];
-    let mut shared_secret: Vec<u8> = vec![0; OQS_KEM_kyber_512_length_shared_secret as usize];
-    unsafe {
-        OQS_KEM_kyber_512_encaps(ciphertext.as_mut_ptr(), shared_secret.as_mut_ptr(), decoded_pk.as_ptr());
-    }
-
-    let ciphertext_b64: String = BASE64_STANDARD.encode(ciphertext);
-    let shared_secret_b64: String = BASE64_STANDARD.encode(shared_secret);
-
-    let result = EncapsulationResult {
-        ciphertext: ciphertext_b64,
-        secret: shared_secret_b64,
-    };
-    serde_wasm_bindgen::to_value(&result).unwrap()
+    JsValue::from_str("To be implemented")
 }
